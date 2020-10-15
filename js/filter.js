727f4a7904cdcc995f4b1ed6253bdfb342db154f
Vitaly Olenichev <Vitaly@MacBook-Vitaly.local>
2020-10-15 13:28:17 +0300

    Личный проект: перламутровые пуговицы (часть 1)

---
+++ js/filter.js
@@ -0,0 +1,26 @@
+'use strict';
+
+(function () {
+
+  const RANDOM_PICTURE_OBJECTS_LENGTH = 10;
+  const getRandomInt = (min, max) => {
+    return Math.round(Math.random() * (max - min)) + min;
+  };
+  const getRandomPictureObjects = (pictureObjects) => {
+    let randomPictureObjects = [];
+    let pictureObjectsCopy = pictureObjects.slice();
+    for (let i = 0; i < RANDOM_PICTURE_OBJECTS_LENGTH; i++) {
+      let randomIndex = getRandomInt(0, pictureObjectsCopy.length - 1);
+      randomPictureObjects.push(pictureObjectsCopy[randomIndex]);
+      pictureObjectsCopy.splice(randomIndex, 1);
+    }
+    return randomPictureObjects;
+  };
+  const sortByCommentsPictureObjects = (pictureObjects) => {
+    return pictureObjects.slice().sort((left, right) => right.comments.length - left.comments.length);
+  };
+  window.filter = {
+    getRandomPictureObjects,
+    sortByCommentsPictureObjects
+  };
+})();
